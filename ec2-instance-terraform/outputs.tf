c50a70b416214370add679d922512961d8af119e
Howler01 <josh@Mac.attlocal.net>
2025-02-20 14:28:37 -0500

    remove

--- ec2-instance-terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = module.ec2.instance_id
-}
-
-output "public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = module.ec2.public_ip
-}

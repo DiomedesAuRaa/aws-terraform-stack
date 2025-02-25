8022aa962ab97f5907f85485d8faef6858e7cc61
Howler01 <40499523+DiomedesAuRaa@users.noreply.github.com>
2025-02-25 09:48:08 -0500

    updates

--- eks/eks.tf
+++ eks/eks.tf
@@ -2,7 +2,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 19.0"
 
-  cluster_name    = "my-eks-cluster"
+  cluster_name    = "iron-gold-eks"
   cluster_version = "1.30"
   subnet_ids      = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
@@ -16,5 +16,15 @@ module "eks" {
       instance_types = ["t3.medium"]
       capacity_type  = "ON_DEMAND"
     }
+  }
+}
+
+
+# Override the aws_cloudwatch_log_group resource
+resource "aws_cloudwatch_log_group" "eks_log_group" {
+  name = "/aws/eks/iron-gold-eks/cluster"
+
+  lifecycle {
+    create_before_destroy = true
   }
 }

b311ae183cde485aee9d59868cacbd2be44bdbf4
Howler01 <40499523+DiomedesAuRaa@users.noreply.github.com>
2025-03-02 15:36:18 -0500

    adding data field

--- load-balancer/outputs.tf
+++ load-balancer/outputs.tf
@@ -7,9 +7,6 @@ output "public_subnet_ids" {
     aws_subnet.subnet_b.id
   ]
 }
-output "alb_sg_id" {
-  value = aws_security_group.alb_sg.id
-}
 output "alb_dns_name" {
   value = aws_lb.ecs_alb.dns_name
 }

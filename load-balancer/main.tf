09d01d635648194dadb45d6f7c3ee98c4a87f01f
Howler01 <40499523+DiomedesAuRaa@users.noreply.github.com>
2025-03-02 15:35:20 -0500

    adding data field

--- load-balancer/main.tf
+++ load-balancer/main.tf
@@ -23,24 +23,8 @@ resource "aws_subnet" "subnet_b" {
 }
 
 # Security Group for ALB
-resource "aws_security_group" "alb_sg" {
-  name        = "ecs-alb-sg"
-  description = "Allow HTTP traffic on port 5000"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Publicly accessible
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+data "aws_security_group" "alb_sg" {
+  name = "ecs-alb-sg"
 }
 
 # Application Load Balancer (ALB)
@@ -48,7 +32,8 @@ resource "aws_lb" "ecs_alb" {
   name               = "ecs-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
+  security_groups    = [data.aws_security_group.alb_sg.id]
+
 
   # Use directly created subnet IDs (not data source lookups)
   subnets = [
